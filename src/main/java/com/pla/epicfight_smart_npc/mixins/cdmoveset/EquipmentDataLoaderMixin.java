c35a42094f3bc2900a340371a4b0bfb2fa7d2004
PlaIsMe <phongvulai96@gmail.com>
2026-07-07 20:10:26 +0700

    1.0: Add netherite gear and adjust difficult with gear

--- src/main/java/com/pla/epicfight_smart_npc/mixins/cdmoveset/EquipmentDataLoaderMixin.java
+++ src/main/java/com/pla/epicfight_smart_npc/mixins/cdmoveset/EquipmentDataLoaderMixin.java
@@ -23,11 +23,13 @@ public abstract class EquipmentDataLoaderMixin {
             "cdmoveset:s_iron_dagger",
             "cdmoveset:s_golden_dagger",
             "cdmoveset:s_diamond_dagger",
+            "cdmoveset:s_netherite_dagger",
             "epicfight:wooden_dagger",
             "epicfight:stone_dagger",
             "epicfight:iron_dagger",
             "epicfight:golden_dagger",
-            "epicfight:diamond_dagger"
+            "epicfight:diamond_dagger",
+            "epicfight:netherite_dagger"
     );
 
     private static final List<String> EPICFIGHT_SMART_NPC$S_GREATSWORD_OFFHAND_POOL = List.of(
@@ -36,16 +38,19 @@ public abstract class EquipmentDataLoaderMixin {
             "cdmoveset:s_iron_greatsword",
             "cdmoveset:s_golden_greatsword",
             "cdmoveset:s_diamond_greatsword",
+            "cdmoveset:s_netherite_greatsword",
             "epicfight:wooden_greatsword",
             "epicfight:stone_greatsword",
             "epicfight:iron_greatsword",
             "epicfight:golden_greatsword",
             "epicfight:diamond_greatsword",
+            "epicfight:netherite_greatsword",
             "wom:wooden_greataxe",
             "wom:stone_greataxe",
             "wom:iron_greataxe",
             "wom:golden_greataxe",
-            "wom:diamond_greataxe"
+            "wom:diamond_greataxe",
+            "wom:netherite_greataxe"
     );
 
     private static final List<String> EPICFIGHT_SMART_NPC$S_SWORD_OFFHAND_POOL = List.of(
@@ -54,11 +59,13 @@ public abstract class EquipmentDataLoaderMixin {
             "minecraft:iron_sword",
             "minecraft:golden_sword",
             "minecraft:diamond_sword",
+            "minecraft:netherite_sword",
             "cdmoveset:s_wooden_sword",
             "cdmoveset:s_stone_sword",
             "cdmoveset:s_iron_sword",
             "cdmoveset:s_golden_sword",
-            "cdmoveset:s_diamond_sword"
+            "cdmoveset:s_diamond_sword",
+            "cdmoveset:s_netherite_sword"
     );
 
     @Inject(method = "canUseCompatDualWeapon", at = @At("HEAD"), cancellable = true)
